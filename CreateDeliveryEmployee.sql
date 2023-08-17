f7799b7b8d59e60600b4f7066e0b59a715ca4516
Mateen Parkar <mateenparkar4@gmail.com>
2023-08-17 12:22:18 +0100

    Created delivery employee table and normalised

---
+++ CreateDeliveryEmployee.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+DROP PROCEDURE IF EXISTS create_deliveryEmployee $$
+CREATE PROCEDURE create_deliveryEmployee()
+BEGIN
+    START TRANSACTION;
+		CREATE TABLE Kainoos_AdamU.DeliveryEmployee (
+		DeliveryEmployeeID INT PRIMARY KEY AUTO_INCREMENT,
+		EmployeeID SMALLINT UNSIGNED NOT NULL, 
+		EmployeeBankInfoID SMALLINT UNSIGNED NOT NULL, 
+		FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID),
+		FOREIGN KEY (EmployeeBankInfoID) REFERENCES EmployeeBankInfo(EmployeeBankInfoID)
+		);
+    END $$
+DELIMITER ;
+CALL create_deliveryEmployee();
